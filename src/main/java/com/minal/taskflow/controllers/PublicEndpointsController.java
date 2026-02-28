1cbe8d2a2b13d3883da972599834da8022de79f9
Minal Singh <minal@Minals-MacBook-Air.local>
2026-02-28 21:15:06 +0530

    INITIAL: Project setup

---
+++ src/main/java/com/minal/taskflow/controllers/PublicEndpointsController.java
@@ -0,0 +1,14 @@
+package com.minal.taskflow.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublicEndpointsController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("Ok. Working fine.");
+    }
+}
